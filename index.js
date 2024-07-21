49758cabadc4e688b24288aaa10bf3a4b19d91a2
Isaac Odiwuor Odhiambo <odhiamboisaach@gmail.com>
2024-07-21 18:04:26 +0300

    var

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Write your solution in this file!
+// Declare customerName in the global scope and set it to 'bob'
+var customerName = 'bob';
+
+// Function to convert customerName to uppercase
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+// Function to set bestCustomer to 'not bob'
+function setBestCustomer() {
+  bestCustomer = 'not bob';
+}
+
+// Function to overwrite the value of bestCustomer
+function overwriteBestCustomer() {
+  bestCustomer = 'maybe bob';
+}
+
+// Declare a constant leastFavoriteCustomer
+const leastFavoriteCustomer = 'some initial value';
+
+// Function to try and change leastFavoriteCustomer (should throw an error)
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'new value'; // This will throw an error because leastFavoriteCustomer is a const
+}
